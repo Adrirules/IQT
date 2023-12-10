fb92a484acec5cc0d982ab94238ccd9010c288e3
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2023-12-10 11:43:07 +0100

    generate iqtest model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_09_144450) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_10_103931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "iq_tests", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
