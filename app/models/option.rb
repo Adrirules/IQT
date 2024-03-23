790e4b04bf7b06dbbc052ec8fa2d33da6b54a04e
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2024-03-23 12:15:35 +0100

    update models question and option with cloudinary + active storage with cloudinary

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,5 +1,7 @@
 class Option < ApplicationRecord
   belongs_to :question
+  has_one_attached :image
+
 
   validates :reponse, presence: true
   validates :isreponsecorrect, inclusion: { in: [true, false] }
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :iqtest
   has_many :options, dependent: :destroy
+  has_one_attached :imageq
+
 
   validates :contentq, presence: true
   accepts_nested_attributes_for :options, allow_destroy: true
