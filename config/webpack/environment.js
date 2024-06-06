6d5e1e41a2eb085508e152db064feece99c66a10
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2024-06-06 11:19:14 +0200

    Fix Webpack 5 configuration in Webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
 
+// Ajouter la configuration de node pour Webpack 5
+environment.config.merge({
+  node: {
+    __dirname: false,
+    __filename: false,
+    global: true,
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  }
+});
+
 module.exports = environment
