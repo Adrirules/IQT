49ae16c54c83a850cd0770c75b27c0d52a2b55c9
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2024-06-06 11:34:51 +0200

    Fix webpack configuration and ensure webpack-cli installation

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,11 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-// Ajouter la configuration de node pour Webpack 5
 environment.config.merge({
   node: {
-    __dirname: false,
-    __filename: false,
     global: true,
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
+    __filename: 'mock',
+    __dirname: 'mock'
   }
 });
 
-module.exports = environment
+module.exports = environment;
